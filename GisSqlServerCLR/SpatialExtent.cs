16ecd712bfd5cd9e6906ac3235a96bf6feb5dbcd
Mahdi Jalali <Mahdi.Jalali9087@gmail.com>
2024-12-28 19:01:51 +0330

    feat : table extent (box)

---
+++ GisSqlServerCLR/SpatialExtent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using Microsoft.SqlServer.Types;
+
+[Serializable]
+[SqlUserDefinedAggregate(
+    Format.UserDefined,
+    IsInvariantToNulls = true,
+    IsInvariantToDuplicates = false,
+    IsInvariantToOrder = true,
+    MaxByteSize = 8000)]
+public class SpatialExtent : IBinarySerialize
+{
+    private double _minX;
+    private double _minY;
+    private double _maxX;
+    private double _maxY;
+    private bool _hasValues;
+
+
+    public void Init()
+    {
+        _minX = double.MaxValue;
+        _minY = double.MaxValue;
+        _maxX = double.MinValue;
+        _maxY = double.MinValue;
+        _hasValues = false;
+    }
+
+    public void Accumulate(SqlGeometry geometry)
+    {
+        if (geometry == null || geometry.IsNull)
+            return;
+
+        _minX = Math.Min(_minX, geometry.STEnvelope().STPointN(1).STX.Value);
+        _minY = Math.Min(_minY, geometry.STEnvelope().STPointN(1).STY.Value);
+        _maxX = Math.Max(_maxX, geometry.STEnvelope().STPointN(3).STX.Value);
+        _maxY = Math.Max(_maxY, geometry.STEnvelope().STPointN(3).STY.Value);
+
+        _hasValues = true;
+    }
+
+    public void Merge(SpatialExtent group)
+    {
+        if (group._hasValues)
+        {
+            _minX = Math.Min(_minX, group._minX);
+            _minY = Math.Min(_minY, group._minY);
+            _maxX = Math.Max(_maxX, group._maxX);
+            _maxY = Math.Max(_maxY, group._maxY);
+            _hasValues = true;
+        }
+    }
+
+    public SqlString Terminate()
+    {
+
+        return new SqlString($"BOX({_minX} {_minY}, {_maxX} {_maxY})");
+    }
+
+    public void Read(System.IO.BinaryReader r)
+    {
+        _minX = r.ReadDouble();
+        _minY = r.ReadDouble();
+        _maxX = r.ReadDouble();
+        _maxY = r.ReadDouble();
+        _hasValues = r.ReadBoolean();
+    }
+
+    public void Write(System.IO.BinaryWriter w)
+    {
+        w.Write(_minX);
+        w.Write(_minY);
+        w.Write(_maxX);
+        w.Write(_maxY);
+        w.Write(_hasValues);
+    }
+}
--- GisSqlServerCLR/SpatialReprojection.cs
+++ GisSqlServerCLR/SpatialReprojection.cs
@@ -5,31 +5,31 @@ using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 using Microsoft.SqlServer.Types;
 
-    public class SpatialReprojection
-    {
+public class SpatialReprojection
+{
     [SqlFunction(
         IsDeterministic = true,
         IsPrecise = true)]
     public static SqlString TransformWktGeometry(string geometry, int srcProj, int dstProj)
-        {
-            string processedGeometry = ProcessWktGeometry(geometry, srcProj, dstProj);
+    {
+        string processedGeometry = ProcessWktGeometry(geometry, srcProj, dstProj);
         return new SqlString(processedGeometry);
-        }
+    }
 
     [SqlFunction(
         IsDeterministic = true,
         IsPrecise = true)]
-        public static SqlGeometry TransformGeometry(SqlGeometry geometry, int dstProj)
-        {
-            SqlGeometry processedGeometry = ProcessGeometry(geometry, dstProj);
-            return processedGeometry;
-        }
+    public static SqlGeometry TransformGeometry(SqlGeometry geometry, int dstProj)
+    {
+        SqlGeometry processedGeometry = ProcessGeometry(geometry, dstProj);
+        return processedGeometry;
+    }
 
-        /////////////////////////////////////////////
-        
-        private static SqlGeometry ProcessGeometry(SqlGeometry geometry, int destinationProj4)
-        {
-            SqlInt32 sqkSrid = geometry.STSrid;
+    /////////////////////////////////////////////
+
+    private static SqlGeometry ProcessGeometry(SqlGeometry geometry, int destinationProj4)
+    {
+        SqlInt32 sqkSrid = geometry.STSrid;
         int srid = sqkSrid.Value;
 
         if (srid == destinationProj4)
@@ -37,94 +37,94 @@ using Microsoft.SqlServer.Types;
             return geometry;
         }
 
-            SqlChars sqlWkt = geometry.STAsText();
-            string wkt = sqlWkt.ToSqlString().Value;
+        SqlChars sqlWkt = geometry.STAsText();
+        string wkt = sqlWkt.ToSqlString().Value;
 
-            string processedGeometry = ProcessWktGeometry(wkt, srid, destinationProj4);
+        string processedGeometry = ProcessWktGeometry(wkt, srid, destinationProj4);
 
-            SqlGeometry geom = SqlGeometry.STGeomFromText(new SqlChars(processedGeometry), destinationProj4);
+        SqlGeometry geom = SqlGeometry.STGeomFromText(new SqlChars(processedGeometry), destinationProj4);
 
-            return geom;
-        }
+        return geom;
+    }
 
-        private static string ProcessWktGeometry(string geometry, int sourceProj4, int destinationProj4)
+    private static string ProcessWktGeometry(string geometry, int sourceProj4, int destinationProj4)
+    {
+        int startIndex = geometry.IndexOf('(');
+        string geometryContent = geometry.Substring(startIndex).Trim();
+
+        string[] coordinateGroups = geometryContent.Split(',');
+
+        foreach (var group in coordinateGroups)
         {
-            int startIndex = geometry.IndexOf('(');
-            string geometryContent = geometry.Substring(startIndex).Trim();
+            string cleanedGroup = CleanCoordinateGroup(group);
+            string[] coordinates = cleanedGroup.Split(' ');
 
-            string[] coordinateGroups = geometryContent.Split(',');
-
-            foreach (var group in coordinateGroups)
+            for (int i = 0; i < coordinates.Length - 1; i += 2)
             {
-                string cleanedGroup = CleanCoordinateGroup(group);
-                string[] coordinates = cleanedGroup.Split(' ');
+                string originalPair = $"{coordinates[i]} {coordinates[i + 1]}";
 
-                for (int i = 0; i < coordinates.Length - 1; i += 2)
-                {
-                    string originalPair = $"{coordinates[i]} {coordinates[i + 1]}";
+                string transformedPair = TransformCoordinatePair(
+                    double.Parse(coordinates[i]),
+                    double.Parse(coordinates[i + 1]),
+                    sourceProj4,
+                    destinationProj4
+                );
 
-                    string transformedPair = TransformCoordinatePair(
-                        double.Parse(coordinates[i]),
-                        double.Parse(coordinates[i + 1]),
-                        sourceProj4,
-                        destinationProj4
-                    );
-
-                    geometry = geometry.Replace(originalPair, transformedPair);
-                }
+                geometry = geometry.Replace(originalPair, transformedPair);
             }
-
-            return geometry;
         }
 
-        private static string CleanCoordinateGroup(string group)
+        return geometry;
+    }
+
+    private static string CleanCoordinateGroup(string group)
+    {
+        return group.Replace('(', ' ').Replace(')', ' ').Trim();
+    }
+
+    private static string TransformCoordinatePair(double x, double y, int sourceProj4, int destinationProj4)
+    {
+        double[] coordinates = { x, y };
+        double[] z = { 0 };
+
+        var sourceProjection = GetProjectionFromEpsg(sourceProj4);
+        var destinationProjection = GetProjectionFromEpsg(destinationProj4);
+
+        DotSpatial.Projections.Reproject.ReprojectPoints(coordinates, z, sourceProjection, destinationProjection, 0, 1);
+
+        return $"{coordinates[0]} {coordinates[1]}";
+    }
+
+    private static DotSpatial.Projections.ProjectionInfo GetProjectionFromEpsg(int epsgCode)
+    {
+        switch (epsgCode)
         {
-            return group.Replace('(', ' ').Replace(')', ' ').Trim();
-        }
+            // EPSG:4326 - WGS84 (Geographic)
+            case 4326:
+                return DotSpatial.Projections.KnownCoordinateSystems.Geographic.World.WGS1984;
 
-        private static string TransformCoordinatePair(double x, double y, int sourceProj4, int destinationProj4)
-        {
-            double[] coordinates = { x, y };
-            double[] z = { 0 };
+            // EPSG:3857 - Web Mercator
+            case 3857:
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.World.WebMercator;
 
-            var sourceProjection = GetProjectionFromEpsg(sourceProj4);
-            var destinationProjection = GetProjectionFromEpsg(destinationProj4);
+            // UTM Zones (Northern Hemisphere)
+            case 32638: // UTM Zone 38N
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone38N;
 
-            DotSpatial.Projections.Reproject.ReprojectPoints(coordinates, z, sourceProjection, destinationProjection, 0, 1);
+            case 32639: // UTM Zone 39N
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone39N;
 
-            return $"{coordinates[0]} {coordinates[1]}";
-        }
+            case 32640: // UTM Zone 40N
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone40N;
 
-        private static DotSpatial.Projections.ProjectionInfo GetProjectionFromEpsg(int epsgCode)
-        {
-            switch (epsgCode)
-            {
-                // EPSG:4326 - WGS84 (Geographic)
-                case 4326:
-                    return DotSpatial.Projections.KnownCoordinateSystems.Geographic.World.WGS1984;
+            case 32641: // UTM Zone 41N
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone41N;
 
-                // EPSG:3857 - Web Mercator
-                case 3857:
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.World.WebMercator;
+            case 32642: // UTM Zone 42N
+                return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone42N;
 
-                // UTM Zones (Northern Hemisphere)
-                case 32638: // UTM Zone 38N
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone38N;
-
-                case 32639: // UTM Zone 39N
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone39N;
-
-                case 32640: // UTM Zone 40N
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone40N;
-
-                case 32641: // UTM Zone 41N
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone41N;
-
-                case 32642: // UTM Zone 42N
-                    return DotSpatial.Projections.KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone42N;
-
-                default:
-                    throw new ArgumentException($"Unsupported EPSG code: {epsgCode}");
-            }
+            default:
+                throw new ArgumentException($"Unsupported EPSG code: {epsgCode}");
         }
     }
+}
