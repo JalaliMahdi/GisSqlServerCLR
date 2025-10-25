70f9039f0bb7068409755c09d8b7988b5315253c
Mahdi Jalali <mahdi.jalali9087@gmail.com>
2025-10-25 15:06:48 +0330

    feat : add multi collection support

--- GisSqlServerCLR/SpatialReprojection.cs
+++ GisSqlServerCLR/SpatialReprojection.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.Globalization;
+using System.Text;
 using Microsoft.SqlServer.Server;
 using Microsoft.SqlServer.Types;
 
@@ -12,8 +12,28 @@ public class SpatialReprojection
         IsPrecise = true)]
     public static SqlString TransformWktGeometry(string geometry, int srcProj, int dstProj)
     {
-        string processedGeometry = ProcessWktGeometry(geometry, srcProj, dstProj);
-        return new SqlString(processedGeometry);
+        if (string.IsNullOrWhiteSpace(geometry))
+        {
+            return SqlString.Null;
+        }
+
+        try
+        {
+            SqlGeometry geom = SqlGeometry.STGeomFromText(new SqlChars(geometry), srcProj);
+            
+            SqlGeometry transformed = TransformGeometry(geom, dstProj);
+            
+            if (transformed.IsNull)
+            {
+                return SqlString.Null;
+            }
+            
+            return transformed.STAsText().ToSqlString();
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException($"Error transforming WKT geometry: {ex.Message}", ex);
+        }
     }
 
     [SqlFunction(
@@ -21,78 +41,283 @@ public class SpatialReprojection
         IsPrecise = true)]
     public static SqlGeometry TransformGeometry(SqlGeometry geometry, int dstProj)
     {
-        SqlGeometry processedGeometry = ProcessGeometry(geometry, dstProj);
-        return processedGeometry;
+        if (geometry == null || geometry.IsNull)
+        {
+            return SqlGeometry.Null;
+        }
+
+        try
+        {
+            SqlInt32 sqlSrid = geometry.STSrid;
+            int srcProj = sqlSrid.Value;
+
+            if (srcProj == dstProj)
+            {
+                return geometry;
+            }
+
+            return ProcessGeometry(geometry, srcProj, dstProj);
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException($"Error transforming geometry: {ex.Message}", ex);
+        }
     }
 
     /////////////////////////////////////////////
 
-    private static SqlGeometry ProcessGeometry(SqlGeometry geometry, int destinationProj4)
+    private static SqlGeometry ProcessGeometry(SqlGeometry geometry, int sourceProj, int destinationProj)
     {
-        SqlInt32 sqkSrid = geometry.STSrid;
-        int srid = sqkSrid.Value;
+        string geometryType = geometry.STGeometryType().Value;
 
-        if (srid == destinationProj4)
+        switch (geometryType.ToUpper())
         {
-            return geometry;
+            case "POINT":
+                return TransformPoint(geometry, sourceProj, destinationProj);
+            
+            case "LINESTRING":
+                return TransformLineString(geometry, sourceProj, destinationProj);
+            
+            case "POLYGON":
+                return TransformPolygon(geometry, sourceProj, destinationProj);
+            
+            case "MULTIPOINT":
+                return TransformMultiPoint(geometry, sourceProj, destinationProj);
+            
+            case "MULTILINESTRING":
+                return TransformMultiLineString(geometry, sourceProj, destinationProj);
+            
+            case "MULTIPOLYGON":
+                return TransformMultiPolygon(geometry, sourceProj, destinationProj);
+            
+            case "GEOMETRYCOLLECTION":
+                return TransformGeometryCollection(geometry, sourceProj, destinationProj);
+            
+            default:
+                throw new NotSupportedException($"Geometry type '{geometryType}' is not supported");
         }
-
-        SqlChars sqlWkt = geometry.STAsText();
-        string wkt = sqlWkt.ToSqlString().Value;
-
-        string processedGeometry = ProcessWktGeometry(wkt, srid, destinationProj4);
-
-        SqlGeometry geom = SqlGeometry.STGeomFromText(new SqlChars(processedGeometry), destinationProj4);
-
-        return geom;
     }
 
-    private static string ProcessWktGeometry(string geometry, int sourceProj4, int destinationProj4)
+    private static SqlGeometry TransformPoint(SqlGeometry point, int sourceProj, int destinationProj)
     {
-        int startIndex = geometry.IndexOf('(');
-        string geometryContent = geometry.Substring(startIndex).Trim();
+        double x = point.STX.Value;
+        double y = point.STY.Value;
 
-        string[] coordinateGroups = geometryContent.Split(',');
+        TransformCoordinate(ref x, ref y, sourceProj, destinationProj);
 
-        foreach (var group in coordinateGroups)
+        string wkt = FormatPoint(x, y);
+        return SqlGeometry.STGeomFromText(new SqlChars(wkt), destinationProj);
+    }
+
+    private static SqlGeometry TransformLineString(SqlGeometry lineString, int sourceProj, int destinationProj)
+    {
+        int numPoints = lineString.STNumPoints().Value;
+        StringBuilder wktBuilder = new StringBuilder("LINESTRING (");
+
+        for (int i = 1; i <= numPoints; i++)
         {
-            string cleanedGroup = CleanCoordinateGroup(group);
-            string[] coordinates = cleanedGroup.Split(' ');
+            SqlGeometry point = lineString.STPointN(i);
+            double x = point.STX.Value;
+            double y = point.STY.Value;
 
-            for (int i = 0; i < coordinates.Length - 1; i += 2)
+            TransformCoordinate(ref x, ref y, sourceProj, destinationProj);
+
+            if (i > 1)
             {
-                string originalPair = $"{coordinates[i]} {coordinates[i + 1]}";
-
-                string transformedPair = TransformCoordinatePair(
-                    double.Parse(coordinates[i]),
-                    double.Parse(coordinates[i + 1]),
-                    sourceProj4,
-                    destinationProj4
-                );
-
-                geometry = geometry.Replace(originalPair, transformedPair);
+                wktBuilder.Append(", ");
             }
+            wktBuilder.Append(FormatCoordinate(x, y));
         }
 
-        return geometry;
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
     }
 
-    private static string CleanCoordinateGroup(string group)
+    private static SqlGeometry TransformPolygon(SqlGeometry polygon, int sourceProj, int destinationProj)
     {
-        return group.Replace('(', ' ').Replace(')', ' ').Trim();
+        StringBuilder wktBuilder = new StringBuilder("POLYGON (");
+
+        SqlGeometry exteriorRing = polygon.STExteriorRing();
+        wktBuilder.Append(TransformRing(exteriorRing, sourceProj, destinationProj));
+
+        int numInteriorRings = polygon.STNumInteriorRing().Value;
+        for (int i = 1; i <= numInteriorRings; i++)
+        {
+            wktBuilder.Append(", ");
+            SqlGeometry interiorRing = polygon.STInteriorRingN(i);
+            wktBuilder.Append(TransformRing(interiorRing, sourceProj, destinationProj));
+        }
+
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
     }
 
-    private static string TransformCoordinatePair(double x, double y, int sourceProj4, int destinationProj4)
+    private static SqlGeometry TransformMultiPoint(SqlGeometry multiPoint, int sourceProj, int destinationProj)
+    {
+        int numGeometries = multiPoint.STNumGeometries().Value;
+        StringBuilder wktBuilder = new StringBuilder("MULTIPOINT (");
+
+        for (int i = 1; i <= numGeometries; i++)
+        {
+            SqlGeometry point = multiPoint.STGeometryN(i);
+            double x = point.STX.Value;
+            double y = point.STY.Value;
+
+            TransformCoordinate(ref x, ref y, sourceProj, destinationProj);
+
+            if (i > 1)
+            {
+                wktBuilder.Append(", ");
+            }
+            wktBuilder.Append($"({FormatCoordinate(x, y)})");
+        }
+
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
+    }
+
+    private static SqlGeometry TransformMultiLineString(SqlGeometry multiLineString, int sourceProj, int destinationProj)
+    {
+        int numGeometries = multiLineString.STNumGeometries().Value;
+        StringBuilder wktBuilder = new StringBuilder("MULTILINESTRING (");
+
+        for (int i = 1; i <= numGeometries; i++)
+        {
+            if (i > 1)
+            {
+                wktBuilder.Append(", ");
+            }
+
+            SqlGeometry lineString = multiLineString.STGeometryN(i);
+            int numPoints = lineString.STNumPoints().Value;
+            wktBuilder.Append("(");
+
+            for (int j = 1; j <= numPoints; j++)
+            {
+                SqlGeometry point = lineString.STPointN(j);
+                double x = point.STX.Value;
+                double y = point.STY.Value;
+
+                TransformCoordinate(ref x, ref y, sourceProj, destinationProj);
+
+                if (j > 1)
+                {
+                    wktBuilder.Append(", ");
+                }
+                wktBuilder.Append(FormatCoordinate(x, y));
+            }
+
+            wktBuilder.Append(")");
+        }
+
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
+    }
+
+    private static SqlGeometry TransformMultiPolygon(SqlGeometry multiPolygon, int sourceProj, int destinationProj)
+    {
+        int numGeometries = multiPolygon.STNumGeometries().Value;
+        StringBuilder wktBuilder = new StringBuilder("MULTIPOLYGON (");
+
+        for (int i = 1; i <= numGeometries; i++)
+        {
+            if (i > 1)
+            {
+                wktBuilder.Append(", ");
+            }
+
+            SqlGeometry polygon = multiPolygon.STGeometryN(i);
+            wktBuilder.Append("(");
+
+            // Exterior Ring
+            SqlGeometry exteriorRing = polygon.STExteriorRing();
+            wktBuilder.Append(TransformRing(exteriorRing, sourceProj, destinationProj));
+
+            // Interior Rings
+            int numInteriorRings = polygon.STNumInteriorRing().Value;
+            for (int j = 1; j <= numInteriorRings; j++)
+            {
+                wktBuilder.Append(", ");
+                SqlGeometry interiorRing = polygon.STInteriorRingN(j);
+                wktBuilder.Append(TransformRing(interiorRing, sourceProj, destinationProj));
+            }
+
+            wktBuilder.Append(")");
+        }
+
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
+    }
+
+    private static SqlGeometry TransformGeometryCollection(SqlGeometry geometryCollection, int sourceProj, int destinationProj)
+    {
+        int numGeometries = geometryCollection.STNumGeometries().Value;
+        StringBuilder wktBuilder = new StringBuilder("GEOMETRYCOLLECTION (");
+
+        for (int i = 1; i <= numGeometries; i++)
+        {
+            if (i > 1)
+            {
+                wktBuilder.Append(", ");
+            }
+
+            SqlGeometry geometry = geometryCollection.STGeometryN(i);
+            SqlGeometry transformedGeom = ProcessGeometry(geometry, sourceProj, destinationProj);
+            
+            string wkt = transformedGeom.STAsText().ToSqlString().Value;
+            wktBuilder.Append(wkt);
+        }
+
+        wktBuilder.Append(")");
+        return SqlGeometry.STGeomFromText(new SqlChars(wktBuilder.ToString()), destinationProj);
+    }
+
+    private static string TransformRing(SqlGeometry ring, int sourceProj, int destinationProj)
+    {
+        int numPoints = ring.STNumPoints().Value;
+        StringBuilder ringBuilder = new StringBuilder("(");
+
+        for (int i = 1; i <= numPoints; i++)
+        {
+            SqlGeometry point = ring.STPointN(i);
+            double x = point.STX.Value;
+            double y = point.STY.Value;
+
+            TransformCoordinate(ref x, ref y, sourceProj, destinationProj);
+
+            if (i > 1)
+            {
+                ringBuilder.Append(", ");
+            }
+            ringBuilder.Append(FormatCoordinate(x, y));
+        }
+
+        ringBuilder.Append(")");
+        return ringBuilder.ToString();
+    }
+
+    private static void TransformCoordinate(ref double x, ref double y, int sourceProj, int destinationProj)
     {
         double[] coordinates = { x, y };
         double[] z = { 0 };
 
-        var sourceProjection = GetProjectionFromEpsg(sourceProj4);
-        var destinationProjection = GetProjectionFromEpsg(destinationProj4);
+        var sourceProjection = GetProjectionFromEpsg(sourceProj);
+        var destinationProjection = GetProjectionFromEpsg(destinationProj);
 
         DotSpatial.Projections.Reproject.ReprojectPoints(coordinates, z, sourceProjection, destinationProjection, 0, 1);
 
-        return $"{coordinates[0]} {coordinates[1]}";
+        x = coordinates[0];
+        y = coordinates[1];
+    }
+
+    private static string FormatPoint(double x, double y)
+    {
+        return $"POINT ({FormatCoordinate(x, y)})";
+    }
+
+    private static string FormatCoordinate(double x, double y)
+    {
+        return $"{x.ToString("G17", CultureInfo.InvariantCulture)} {y.ToString("G17", CultureInfo.InvariantCulture)}";
     }
 
     private static DotSpatial.Projections.ProjectionInfo GetProjectionFromEpsg(int epsgCode)
--- Program.cs
+++ Program.cs
@@ -16,6 +16,14 @@ namespace GisSqlCLR
             SqlGeometry geometry = SqlGeometry.STGeomFromText(
                 new System.Data.SqlTypes.SqlChars("POINT (6048669.75554771 3751819.65061306)"), 3857);
 
+            SqlGeometry geometryPolygon = SqlGeometry.STGeomFromText(
+                new System.Data.SqlTypes.SqlChars("POLYGON ((-64.8 32.3, -65.5 18.3, -80.3 25.2, -64.8 32.3))"), 4326);
+
+            SqlGeometry geometryCollection = SqlGeometry.STGeomFromText(
+                new System.Data.SqlTypes.SqlChars("GEOMETRYCOLLECTION(POINT(12.4924 41.8902),LINESTRING(12.4924 41.8902, 12.4960 41.9028),POLYGON((  12.4924 41.8902,  12.4960 41.9028,  12.4800 41.8950,  12.4924 41.8902))\r\n)"), 4326);
+
+            var a2 = SpatialReprojection.TransformGeometry(geometryPolygon, 3857);
+            var a23 = SpatialReprojection.TransformGeometry(geometryCollection, 3857);
 
             var a = SpatialReprojection.TransformGeometry(geometry,  4326);
 
